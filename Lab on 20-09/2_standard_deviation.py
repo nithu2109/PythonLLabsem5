93406bb3fb8931d73135c91e4d30062df716d72c
nithu2109 <nithish.vagu219@gmail.com>
2021-09-24 18:41:37 +0530

    adding 2nd problem of the week

--- Lab on 20-09/2_standard_deviation.py
+++ Lab on 20-09/2_standard_deviation.py
@@ -1,3 +1,13 @@
 import math
+def standard_dev(*n):
+ sum=0
+ for i in n:
+  sum+=i
+  mean=sum/len(n)
+ for i in n:
+  t=0
+  t+=(i-mean)**2
+  p=math.sqrt(t/len(n))
+  return p
 
-def deviation_calc(*args):
+print("The standard deviation:", standard_dev(2,3,4,5))
