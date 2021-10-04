b7747463fd2547c364e8056aeb4d62e61b2c506f
nithu2109 <nithish.vagu219@gmail.com>
2021-10-04 09:33:00 +0530

    added first problem with input

--- Lab on 27-09/1_insert.py
+++ Lab on 27-09/1_insert.py
@@ -1,11 +1,17 @@
+n=int(input("Enter the value of n : "))
+mylist=[]
+for i in range(0,n):
+ i=int(input("Enter the element : "))
+ mylist.append(i)
+mylist.sort()
+print("List before inserting : ")
+print(mylist)
 num=int(input("Enter an Element to Insert : "))
-list=[1,2,3,7,9,10]
-for i in range (len(list)):
-  if list[i]>num:
-     list.insert(i,num)
+for i in range (len(mylist)):
+  if mylist[i]>num:
+     mylist.insert(i,num)
      break
-if list[len(list)-1]<num:
-        print("yep")
-        list.insert(len(list),num)
+if mylist[len(mylist)-1]<num:
+        mylist.insert(len(mylist),num)
 
-print(list)
+print(mylist)
